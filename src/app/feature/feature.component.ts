be3c71c0a6b566457b382d4405bf344e3a069ab0
MahaSudharson <sudhan1328@gmail.com>
2023-10-16 13:41:54 +0530

    add attribute directives

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./feature.component.css']
 })
 export class FeatureComponent {
+  isSpecial: boolean = false;
 
+
+
+  currentStyles: { [key: string]: string } = {
+    'color':  'orange'    
+  };
+  getCssClasses() {
+    this.currentStyles = {
+    };
+    this.isSpecial=!this.isSpecial;
+    return {
+      'special-class': this.isSpecial, // Apply 'special-class' if isSpecial is true
+      'default-class': !this.isSpecial // Apply 'default-class' if isSpecial is false
+    };
+  }
 }
